6036d90fc67bb1d36d0e77baf20e2e3a9e897b3a
Dmytro Nikitiuk <d.nikitiuk@noisyminer.com>
2020-09-11 18:13:18 +0300

    Setup config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { configService } from "./config/config.service";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+  ],
 })
 export class AppModule {}
