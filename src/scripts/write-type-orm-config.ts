eaff4653914d7aa9ed28175ac012b88c1d3e5217
Dmytro Nikitiuk <d.nikitiuk@noisyminer.com>
2020-09-14 19:16:16 +0300

    Setup server starting; Setup Typeorm migrations flow

---
+++ src/scripts/write-type-orm-config.ts
@@ -0,0 +1,7 @@
+import { configService } from '../config/config.service';
+import * as fs from 'fs';
+
+fs.writeFileSync(
+  'ormconfig.json',
+  JSON.stringify(configService.getTypeOrmConfig(), null, 2),
+);
