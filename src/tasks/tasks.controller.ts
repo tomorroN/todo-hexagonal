2905d957a4218742de3a94d548cda464bbbddaaa
Dmytro Nikitiuk <d.nikitiuk@noisyminer.com>
2020-09-21 14:37:25 +0300

    Removed unused controller and module

--- src/tasks/tasks.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-
-@Controller('tasks')
-export class TasksController {
-  constructor(private tasksService: TasksService) {}
-}
